84189e8e43b9a4608e531d116301b670372441e3
nicolafan <48762613+nicolafan@users.noreply.github.com>
2024-01-05 19:23:14 +0100

    Refactor search page layout and add image overlay

--- clipper-frontend/src/pages/SearchPage.vue
+++ clipper-frontend/src/pages/SearchPage.vue
@@ -1,27 +1,46 @@
 <template>
     <div class="container-fluid mt-5">
         <div class="row">
-            <div class="col-4" />
-            <div class="col-4">
+            <div class="col-3" />
+            <div class="col-6">
                 <div class="d-flex justify-content-between">
                     <div class="col-9 pe-2">
                         <input type="text" class="form-control" v-model="searchQuery" placeholder="Enter your query here">
                     </div>
                     <div class="col-3">
-                        <button class="btn btn-primary" @click="search">Make a query</button>
+                        <button class="btn btn-primary" @click="search">Search</button>
                     </div>
                 </div>
             </div>
-            <div class="col-4" />
         </div>
         <div v-if="searchResults" class="gallery mt-5">
             <template v-for="(result, index) in searchResults.ids[0]" :key="index">
                 <div style="position: relative;">
                     <!-- show image only if the url works -->
-                    <img :src="'data:image/jpeg;base64,' + searchResults.images[0][index]" alt="Image">
-                    <div style="position: absolute; bottom: 0; right: 0; background: rgba(0, 0, 0, 0.5); color: white; padding: 5px;">
-                        {{ "Similarity: " + (1 - searchResults.distances[0][index]).toFixed(2) }}
-                    </div>
+                    <img :src="'data:image/jpeg;base64,' + searchResults.images[0][index]" alt="Image"
+                        @mouseover="selectedImage = result; calculateCardPosition($event)"
+                        @mouseleave="selectedImage = null" :class="{ 'hovered-image': selectedImage === result }">
+                    <transition name="fade">
+                        <div v-if="selectedImage === result" class="card image-card" :style="{ [cardPosition]: '0' }"
+                            @mouseover="selectedImage = result;" @mouseleave="selectedImage = null">
+                            <div class="card-header">{{ result }}</div>
+                            <div class="card-body">
+                                <div class="card-text">
+                                    <b>Similarity: </b> {{ (1 - searchResults.distances[0][index]).toFixed(2) }}
+                                    <template v-for="(value, key) in searchResults.metadatas[0][index]" :key="key">
+                                        <div>
+                                            <b>{{ key }}:</b> {{ value }}
+                                        </div>
+                                    </template>
+                                </div>
+                            </div>
+                        </div>
+                    </transition>
+                    <transition name="fade">
+                        <div v-if="selectedImage !== result" class="overlay">
+                            {{ (1 - searchResults.distances[0][index]).toFixed(2) }}
+                        </div>
+                    </transition>
                 </div>
             </template>
         </div>
@@ -45,7 +64,40 @@
     display: block;
     margin-left: auto;
     margin-right: auto;
-    cursor: pointer;
+}
+
+.hovered-image {
+    opacity: 0.8;
+    /* add border of boostrap primary color */
+    border: 6px solid #0d6efd;
+}
+
+.image-card {
+    position: absolute;
+    top: 30%;
+    left: 100%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+    width: max-content;
+}
+
+.overlay {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    background: rgba(0, 0, 0, 0.5);
+    color: white;
+    padding: 5px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
 }
 </style>
 <script>
@@ -56,7 +108,9 @@ export default {
     data() {
         return {
             searchQuery: '',
-            searchResults: null
+            searchResults: null,
+            selectedImage: null,
+            cardPosition: 'right'
         }
     },
     methods: {
@@ -66,17 +120,27 @@ export default {
                     query: this.searchQuery
                 }
             }).then((response) => {
-                this.searchResults = response.data
+                this.searchResults = response.data;
             })
         },
         getImage(id) {
             axios.get('/get_image/' + id)
                 .then(response => {
-                    this.image = response.data.image
+                    this.image = response.data.image;
                 })
                 .catch(error => {
-                    console.log(error)
+                    console.log(error);
                 })
+        },
+        calculateCardPosition(event) {
+            const imageRect = event.target.getBoundingClientRect();
+            const spaceOnRight = window.innerWidth - imageRect.right;
+            const spaceOnLeft = imageRect.left;
+            if (spaceOnRight >= spaceOnLeft) {
+                this.cardPosition = 'right';
+            } else {
+                this.cardPosition = 'left';
+            }
         }
     }
 }
